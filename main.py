193d9477171f7e6b3bd174402e752613bc432c13
JakeMann12 <120700161+JakeMann12@users.noreply.github.com>
2023-12-02 19:10:15 -0500

    plot path and reward function

--- main.py
+++ main.py
@@ -4,30 +4,65 @@ import torch
 from simple_rexy.envs.rexy_env import SimpleRexyEnv
 import time
 import pybullet as p
+import matplotlib.pyplot as plt
+import numpy as np
 
 #zswang666 and electricelephant
 
-def main(): #VeRSION 2 PANDA/Foo
+def plot_results(rewards, xy_positions):
+    # Plot the reward function
+    plt.figure(figsize=(14, 5))
+    
+    plt.subplot(1, 2, 1)
+    plt.plot(rewards, label='Reward')
+    plt.xlabel('Step')
+    plt.ylabel('Reward')
+    plt.title('Reward Function Over Time')
+    plt.legend()
+
+    # Plot the x-y positions
+    xy_positions = list(zip(*xy_positions))
+    plt.subplot(1, 2, 2)
+    colors = np.linspace(0, 1, len(xy_positions[0]))  # Color based on time, green to purple
+    plt.scatter(xy_positions[0], xy_positions[1], c=colors, cmap='viridis', label='Path', marker='o')
+    plt.scatter([xy_positions[0][0]], [xy_positions[1][0]], color='green', marker='*', s = 160, label='Start')
+    plt.scatter([xy_positions[0][-1]], [xy_positions[1][-1]], color='red', marker='x', label='End')
+    plt.xlabel('X Position')
+    plt.ylabel('Y Position')
+    plt.title('Path of Rexy Over Time')
+    plt.legend()
+
+    plt.show()
+
+def main():
     env = gym.make('rexy-v0')
-    env.reset()      #observation = env.reset()
+    observation = env.reset()
+
+    # Lists to store data for plotting
+    rewards = []
+    xy_positions = []
+
     steps = 500
-    for _ in range(steps):
+    for step in range(steps):
         action = env.action_space.sample()
-        #action = [0]*6 #test
-        #print(action)
         observation, reward, done, info = env.step(action)
-        
-        if _ == 1: #comparison
-            print(f"startobs:\n {observation}\n")
-            print('\n'+str(env.rexy.get_observation())+'\n')
+
+        rewards.append(reward)
+        xy_positions.append((observation[0], observation[1]))
+
+        if step == 1:  # Print observation for comparison
+            print(f"Initial Observation:\n{observation}\n")
+            print(f"Rexy Observation:\n{env.rexy.get_observation()}\n")
+
         if not env.observation_space.contains(observation):
             print("Invalid observation:", observation)
-        if _ == steps-1:
-            print(f'got to the end of the for loop! \n{observation}\n')
+
         if done:
+            print(f"Episode finished after {step + 1} timesteps\nFinal Observation:\n{observation}")
             break
 
-    print(f"got to the end point!\n {observation}")
+    # Call the plot_results function to plot the collected data
+    plot_results(rewards, xy_positions)
     env.close()
 
 if __name__ == '__main__':
--- simple_rexy/envs/rexy_env.py
+++ simple_rexy/envs/rexy_env.py
@@ -1,5 +1,3 @@
-#needs heavy mods
-
 import os
 import gym
 from gym import error, spaces, utils
@@ -38,6 +36,7 @@ class SimpleRexyEnv(gym.Env):
         # Reduce length of episodes for RL algorithms
         p.setTimeStep(1/30, self.client)
 
+        self.DEBUG_MODE = False
         self.rexy = None
         self.goal = None
         self.done = False
@@ -47,31 +46,59 @@ class SimpleRexyEnv(gym.Env):
         self.reset()
 
     def step(self, action):
+        print("=====  STEPTIME  =====\n") if self.DEBUG_MODE else None
         p.configureDebugVisualizer(p.COV_ENABLE_SINGLE_STEP_RENDERING)
 
         # Feed action to the rexy and get observation of rexy's state
+        print("Before apply_action") if self.DEBUG_MODE else None
         self.rexy.apply_action(action)
+        print("After apply_action") if self.DEBUG_MODE else None
         p.stepSimulation()
-        
+        print("After stepSimulation") if self.DEBUG_MODE else None
         rexy_ob = self.rexy.get_observation()
+        print("After get_observation") if self.DEBUG_MODE else None
+        reward = self.compute_reward(rexy_ob)
+        print("After compute_reward") if self.DEBUG_MODE else None
+        ob = np.array(rexy_ob + self.goal, dtype=np.float32)
+        print("After array conversion") if self.DEBUG_MODE else None
+        return ob, reward, self.done, dict()
 
+    def compute_reward(self, rexy_ob):
+        print("------COMPUTEREWARD TIME--------\n") if self.DEBUG_MODE else None
         # Compute reward as L2 change in distance to goal
         dist_to_goal = math.sqrt(((rexy_ob[0] - self.goal[0]) ** 2 +
-                                  (rexy_ob[1] - self.goal[1]) ** 2))
+                                (rexy_ob[1] - self.goal[1]) ** 2))
+        
         reward = max(self.prev_dist_to_goal - dist_to_goal, 0)
+
+        """# Additional reward modifications or calculations can be done here
+
+        # Penalize if the robot is not facing forwards
+        forward_direction = np.array([1, 0])  # Assuming your forward direction is along the x-axis
+        current_direction = np.array([math.cos(rexy_ob[2]), math.sin(rexy_ob[2])])
+        alignment_penalty = -10  # Adjust as needed
+
+        alignment_reward = np.dot(forward_direction, current_direction)
+        if alignment_reward < 0.8:  # Example threshold, adjust as needed
+            reward += alignment_penalty
+            print("Alignment penalty applied") if self.DEBUG_MODE else None
+
+        # Penalize if the robot is not upright
+        orientation_quaternion = rexy_ob[1]  # Assuming orientation is the second element
+        orientation_euler = p.getEulerFromQuaternion(orientation_quaternion)
+        tilt_penalty = -10  # Adjust as needed
+
+        # Check the pitch angle (rotation around the y-axis)
+        pitch_angle = orientation_euler[1]
+        if abs(pitch_angle) > 0.2:  # Example threshold, adjust as needed
+            reward += tilt_penalty
+            print("Tilt penalty applied") if self.DEBUG_MODE else None"""
+
+        # Update prev_dist_to_goal for the next step
         self.prev_dist_to_goal = dist_to_goal
 
-        # Done by running off boundaries
-        if (rexy_ob[0] >= 10 or rexy_ob[0] <= -10 or
-                rexy_ob[1] >= 10 or rexy_ob[1] <= -10):
-            self.done = True
-        # Done by reaching goal
-        elif dist_to_goal < 1:
-            self.done = True
-            reward = 50
-
-        ob = np.array(rexy_ob + self.goal, dtype=np.float32)
-        return ob, reward, self.done, dict()
+        print(f"DONE COMPUTING REWARD: {reward:.3f}") if self.DEBUG_MODE else None
+        return reward
 
     def seed(self, seed: int) -> None:
         """Set the seeds for random and numpy
@@ -81,29 +108,45 @@ class SimpleRexyEnv(gym.Env):
         random.seed(seed)
         
     def reset(self):
+        print("=== Resetting Environment ===") if self.DEBUG_MODE else None
+
+        # Reset the simulation and gravity
         p.resetSimulation(self.client)
-        p.setGravity(0, 0, -9.81) #m/s^2
+        p.setGravity(0, 0, -9.81)  # m/s^2
+
         # Reload the plane and rexy
+        print("Loading Plane...") if self.DEBUG_MODE else None
         Plane(self.client)
+
+        print("Creating Rexy...") if self.DEBUG_MODE else None
         self.rexy = Rexy(self.client)
 
         # Set the goal to a random target
-        x = self.np_random.uniform(6, 8) # if self.np_random.integers(0, 2) else self.np_random.uniform(-5, -9) #NOTE: I guess that I can just vary the one distance
-        y = 0 #NOTE: CHANGED HERE
+        x = 7  # Hardcoded value for now
+        y = 0
         self.goal = (x, y)
         self.done = False
 
         # Visual element of the goal
+        print(f"Creating Goal at {self.goal}...") if self.DEBUG_MODE else None
         Goal(self.client, self.goal)
 
         # Get observation to return
         rexy_ob = self.rexy.get_observation()
 
         self.prev_dist_to_goal = math.sqrt(((rexy_ob[0] - self.goal[0]) ** 2 +
-                                           (rexy_ob[1] - self.goal[1]) ** 2))
-        return np.array(rexy_ob + self.goal, dtype=np.float32)
+                                        (rexy_ob[1] - self.goal[1]) ** 2))
 
-    
+        print(f"Prev dist to goal: {self.prev_dist_to_goal:.3f}") if self.DEBUG_MODE else None
+
+        observation = np.array(rexy_ob + self.goal, dtype=np.float32)
+        info = {}
+
+        print(f"Observation: {observation}") if self.DEBUG_MODE else None
+        print(f"Observation: {info}") if self.DEBUG_MODE else None
+        print("=== Reset Complete ===") if self.DEBUG_MODE else None
+
+        return observation, info
     
     def render(self): #, mode='human'): #NOTE: didn't change anything but car-> rexy
         #p.connect(p.GUI)
@@ -130,31 +173,6 @@ class SimpleRexyEnv(gym.Env):
         self.rendered_img.set_data(frame)
         plt.draw()
         plt.pause(.00001)
-    
-    """
-    def render(self, mode='human'):
-        mode = {'render.modes': [mode]}
-        view_matrix = p.computeViewMatrixFromYawPitchRoll(cameraTargetPosition=[0.7,0,0.05],
-                                                            distance=.7,
-                                                            yaw=90,
-                                                            pitch=-70,
-                                                            roll=0,
-                                                            upAxisIndex=2)
-        proj_matrix = p.computeProjectionMatrixFOV(fov=60,
-                                                     aspect=float(960) /720,
-                                                     nearVal=0.1,
-                                                     farVal=100.0)
-        (_, _, px, _, _) = p.getCameraImage(width=960,
-                                              height=720,
-                                              viewMatrix=view_matrix,
-                                              projectionMatrix=proj_matrix,
-                                              renderer=p.ER_BULLET_HARDWARE_OPENGL)
-
-        rgb_array = np.array(px, dtype=np.uint8)
-        rgb_array = np.reshape(rgb_array, (720,960, 4))
-
-        rgb_array = rgb_array[:, :, :3]
-        return rgb_array"""
     
     def close(self):
         p.disconnect(self.client)
