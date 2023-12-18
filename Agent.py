0b1ee2ca6cb1f13fb03c800c556bb45ed1206304
JakeMann12 <120700161+JakeMann12@users.noreply.github.com>
2023-12-18 15:59:01 -0500

    jk batch normalization

--- Agent.py
+++ Agent.py
@@ -22,16 +22,18 @@ class Net(nn.Module):
     def __init__(self, num_states, num_actions):
         super(Net, self).__init__()
         self.fc1 = nn.Linear(num_states, 256)
-        self.relu1 = nn.ReLU()
+        self.bn1 = nn.BatchNorm1d(256)  # Batch normalization for first hidden layer
         self.fc2 = nn.Linear(256, 128)
-        self.relu2 = nn.ReLU()
+        self.bn2 = nn.BatchNorm1d(128)  # Batch normalization for second hidden layer
         self.fc3 = nn.Linear(128, num_actions)
 
     def forward(self, x):
         x = self.fc1(x)
-        x = self.relu1(x)
+        x = self.bn1(x)  # Apply batch normalization before activation
+        x = F.relu(x)
         x = self.fc2(x)
-        x = self.relu2(x)
+        x = self.bn2(x)  # Apply batch normalization before activation
+        x = F.relu(x)
         action_values = self.fc3(x)
         return action_values
 
